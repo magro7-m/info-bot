c3f5d1be1b24221ccac046de6da76c75c6d89a29
magro7-m <magro7.m.96@gmail.com>
2026-05-15 09:18:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ def main():
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(button))
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
